8e23a8848c98980fb31abf26e53c3e55b36c0cfd
Alex Mark <alexthemark@gmail.com>
2022-08-19 14:21:03 -0400

    Create package

---
+++ README.md
@@ -0,0 +1,30 @@
+# Manifest Github Action
+
+This action sends a generated SBOM to a Manifest Cyber account
+
+## Inputs
+
+### `apiKey`
+
+**Required** An API key. Generate this key in the Manifest Cyber app.
+
+### `input`
+
+**Required** The location of a BOM file. Accepts CycloneDX or SPDX SBOMs in JSON, XML, or SPDX tag:value format.
+
+
+## Example usage
+
+```
+- name: Build SBOM
+  uses: CycloneDX/gh-node-module-generatebom@master
+  with:
+    path: "./"
+    output: "./bom.json"
+- name: Transmit own SBOM
+  uses: @manifest/manifest-github-action@main
+  id: transmit
+  with:
+    apiKey: ${{ secrets.MANIFEST_API_KEY }}
+    input: "./bom.json"
+```
---
+++ index.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+const https = require("https");
+const core = require("@actions/core");
+
+try {
+  const apiKey = core.getInput("apiKey");
+  const bomFilename = core.getInput("input");
+
+  const bomContents = fs.readFileSync(bomFilename);
+  const base64BomContents = Buffer.from(bomContents).toString("base64");
+
+  const payload = {
+    apiKey,
+    base64BomContents,
+  };
+
+  const postData = JSON.stringify(payload);
+
+  const requestOptions = {
+    hostname: "mvdryhw7l8.execute-api.us-east-1.amazonaws.com",
+    path: "/prod/receive/",
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "Content-Length": Buffer.byteLength(postData),
+    },
+  };
+
+  console.log("Sending request to Manifest Server");
+
+  const req = https.request(requestOptions, (res) => {
+    const statusCode = res.statusCode;
+    let body = "";
+    if (statusCode >= 200 && statusCode < 300) {
+      console.log("Uploaded to Manifest Server");
+    } else {
+      core.setFailed(
+        "Failed to upload:" + res.statusCode + " " + res.statusMessage
+      );
+    }
+  });
+
+  req.on("error", (e) => {
+    console.error(`Problem with request: ${e.message}`);
+    core.setFailed(e.message);
+  });
+
+  req.write(postData);
+  req.end();
+} catch (error) {
+  core.setFailed(error.message);
+}
