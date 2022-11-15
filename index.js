2c327f43f91a601cf1e7d222c2dd5a6a8a6e4925
Andrew Defee <adefee@users.noreply.github.com>
2022-11-15 17:15:57 -0600

    Push v1 to production (#2)
    * Update to new Manifest API v1
    
    * Update README.md
    
    * 1.0.1
    
    * Update index.js
    
    * Update index.js
    
    * doc cleanup, add bomSource as option

--- index.js
+++ index.js
@@ -1,43 +1,67 @@
 const fs = require("fs");
 const https = require("https");
+const http = require("http");
 const core = require("@actions/core");
+const path = require('path');
 
 try {
   const apiKey = core.getInput("apiKey");
   const bomFilePath = core.getInput("bomFilePath");
-
+  const assetRelationship = core.getInput("relationship");
+  const bomSource = core.getInput("source");
   const bomContents = fs.readFileSync(bomFilePath);
   const base64BomContents = Buffer.from(bomContents).toString("base64");
 
   const payload = {
-    apiKey,
-    base64BomContents,
+    base64BomContents, // Incoming file Buffer
+    relationship: assetRelationship && assetRelationship.length > 0 ? assetRelationship : 'first', // 'first' or 'third'-party
+    filename: bomFilePath, // File name/path. Optional. Used for logging/debugging purposes.
+    source: bomSource && bomSource.length > 0 ? bomSource : 'github-action' // This is stored and visible to you in the Manifest app.
   };
 
   const postData = JSON.stringify(payload);
 
   const requestOptions = {
-    hostname: "mvdryhw7l8.execute-api.us-east-1.amazonaws.com",
-    path: "/prod/receive/",
     method: "PUT",
     headers: {
+      "Authorization": `Bearer ${apiKey}`,
       "Content-Type": "application/json",
       "Content-Length": Buffer.byteLength(postData),
     },
   };
 
   console.log("Sending request to Manifest Server");
+  let req = null;
 
-  const req = https.request(requestOptions, (res) => {
-    const statusCode = res.statusCode;
-    if (statusCode >= 200 && statusCode < 300) {
-      console.log("Uploaded to Manifest Server");
-    } else {
-      core.setFailed(
-        "Failed to upload:" + res.statusCode + " " + res.statusMessage
-      );
-    }
-  });
+  /**
+   * At Manifest, we like to eat our own dogfood - you'll see some development code we use when testing our API and this action locally. You can safely ignore the below `if` statement - the `else` clause will always fire during normal production use of this action. We include our development code for both transparency to you and our own convenience.
+   * If you have a better idea or suggested improvements, fork/PR or ping us at engineering@manifestcyber.com and we'd love to chat over a virtual cup of coffee :)
+   */
+  if (process.env.TEST_LOCALLY && process.env.TEST_LOCALLY === 'enabled') {
+    req = http.request(`http://local.manifestcyber.com:8081/v1/sbom/upload`, requestOptions, (res) => {
+      const statusCode = res.statusCode;
+      if (statusCode >= 200 && statusCode < 300) {
+        console.log("[DEV] Uploaded to Manifest Server");
+      } else {
+        core.setFailed(
+          "[DEV] Failed to upload:" + res.statusCode + " " + res.statusMessage
+        );
+      }
+    });
+  }
+  
+  else {
+    req = https.request(`https://api.manifestcyber.com/v1/sbom/upload`, requestOptions, (res) => {
+      const statusCode = res.statusCode;
+      if (statusCode >= 200 && statusCode < 300) {
+        console.log("Uploaded to Manifest Server");
+      } else {
+        core.setFailed(
+          "Failed to upload:" + res.statusCode + " " + res.statusMessage
+        );
+      }
+    });
+  }
 
   req.on("error", (e) => {
     console.error(`Problem with request: ${e.message}`);
