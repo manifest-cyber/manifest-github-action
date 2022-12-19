03cbf75d2e7257175a1dd1b151844af8b3ab0a15
manifestori <ori@manifestcyber.com>
2022-12-19 16:51:20 +0200

    fix: name defaults

--- update-sbom.sh
+++ update-sbom.sh
@@ -5,7 +5,18 @@ function update_spdx_sbom {
     local name=$2
     local version=$3
 
-    #
+    if [ -z "$name" ]; then
+        name="$GITHUB_REPOSITORY"
+    else
+        name="$name"
+    fi
+
+    if [ -z "$version" ]; then
+        version="${gittag:-v0.0.0-$currentdate-$shortsha}"
+    else
+        version="$version"
+    fi
+
     if (! jq '.relationships[] | select(.relationshipType == "DESCRIBES")' "$filepath") >/dev/null 2>&1; then
         jq --arg name "$name-$version" \
             '.name = $name' \
@@ -23,8 +34,10 @@ function update_spdx_sbom {
 
 function update_cyclonedx_sbom {
     local filepath=$1
-    local name=$2
-    local version=$3
+    local tmpname=$2
+    local tmpversion=$3
+    local name=""
+    local version=""
 
     # Read the input file and parse the JSON
     input=$(cat "$filepath")
@@ -32,23 +45,42 @@ function update_cyclonedx_sbom {
     local existingName=$(echo "$input" | jq -r '.metadata.component.name')
     local existingVersion=$(echo "$input" | jq -r '.metadata.component.version')
 
-    if [ -d "$existingName" ] && [ "$existingVersion" == "null" ]; then
-        json=$(echo "$input" | jq -r '.metadata.component')
+    if [ -z "$tmpname" ]; then
+        name="$GITHUB_REPOSITORY"
+    else
+        name="$tmpname"
+    fi
+
+    if [ -z "$tmpversion" ]; then
+        version="${gittag:-v0.0.0-$currentdate-$shortsha}"
+    else
+        version="$tmpversion"
+    fi
+
+
+    json=$(echo "$input" | jq -r '.metadata.component')
+    if [ ! -z "$tmpname" ] || [ -d "$existingName" ]; then
 
         # Add the name to the "name" field
         json=$(echo "$json" | jq ".name = \"$name\"")
+    else
+        echo "using existing SBOM values for name: $existingName"
+    fi
 
+    if [ ! -z "$tmpversion" ] || [ "$existingVersion" == "null" ]; then
+    
         # Add the version to the "version" field
         json=$(echo "$json" | jq ".version = \"$version\"")
-
-        # Update the input JSON with the updated version
-        input=$(echo "$input" | jq '.metadata.component = $json' --argjson json "$json")
-
-        # Output the updated JSON to a file
-        echo "$input" >"$filepath"
     else
-        echo "using existing SBOM values: name: $existingName version: $existingVersion"
+        echo "using existing SBOM values for version: $existingVersion"
     fi
+
+    # Update the input JSON with the updated version
+    input=$(echo "$input" | jq '.metadata.component = $json' --argjson json "$json")
+
+    # Output the updated JSON to a file
+    echo "$input" >"$filepath"
+
 }
 
 function update_sbom {
@@ -79,8 +111,8 @@ function update_sbom {
 curl https://gist.githubusercontent.com/manifestori/4a6c62617e05fb054a1410a16ea2b29b/raw/43686f969cf4b7a4752cd8992bfd38fbef2e5e48/syft.yaml >syft.yaml
 filename="$SBOM_FILENAME"
 output="$SBOM_OUTPUT"
-tmpname="$SBOM_NAME"
-tmpversion="$SBOM_VERSION"
+name="$SBOM_NAME"
+version="$SBOM_VERSION"
 currentdate=$(date "+%Y%m%d%H%M%S")
 shortsha=$(git rev-parse --short "$GITHUB_SHA")
 
@@ -90,17 +122,4 @@ if [ "$GITHUB_REF_TYPE" = "tag" ]; then
     gittag=$GITHUB_REF_NAME
 fi
 
-if [ -z "$tmpname" ]; then
-    name="$GITHUB_REPOSITORY"
-else
-    name="$tmpname"
-fi
-
-if [ -z "$tmpversion" ]; then
-    version="${gittag:-v0.0.0-$currentdate-$shortsha}"
-else
-    version="$tmpversion"
-fi
-
 update_sbom "$filename" "$name" "$version" "$output"
-
