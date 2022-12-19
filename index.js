c394488e7749d56482165c4e875d08c34a0cdd14
manifestori <ori@manifestcyber.com>
2022-12-19 17:49:35 +0200

    fix: version

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ async function execWrapper(cmd) {
 }
 
 try {
-  const apiKey = core.getInput("apiKey");
+  const apiKey = "2IrZCN1V2XqR474RObSSueMNgR3uhI/746mHWhzHIiE=" // core.getInput("apiKey");
   console.log("apikey", apiKey)
   const bomFilePath = core.getInput("bomFilePath");
   const output = core.getInput("sbom-output");
