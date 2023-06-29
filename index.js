7e3125e84b17075d2c1b44ea022aac2eb2a82d1a
wey-chiang <chiangw@vt.edu>
2023-06-29 16:40:15 -0400

    MFST-607: added core inputs and removed unnecessary execWrapper left in from testing

--- index.js
+++ index.js
@@ -71,22 +71,23 @@ try {
   const output = core.getInput("sbom-output");
   const name = core.getInput("sbom-name");
   const version = core.getInput("sbom-version");
+  const publish = core.getInput("sbom-publish");
+  const generator = core.getInput("sbom-generator");
+  const generatorFlags = core.getInput("sbom-generator-flags");
 
-  execWrapper(`export SBOM_FILENAME=${bomFilePath}; export SBOM_OUTPUT=${output}; export SBOM_NAME=${name}; export SBOM_VERSION=${version};`).then(async () => {
-    execWrapper(`SBOM_FILENAME=${bomFilePath} SBOM_OUTPUT=${output} SBOM_NAME=${name} SBOM_VERSION=${version} bash ${__dirname}/update-sbom.sh`).then(async () => {
-      /**
-       * At Manifest, we like to eat our own dogfood - you'll see some development code we use when testing our API and this action locally. You can safely ignore the below `if` statement - the `else` clause will always fire during normal production use of this action. We include our development code for both transparency to you and our own convenience.
-       * If you have a better idea or suggested improvements, fork/PR or ping us at engineering@manifestcyber.com and we'd love to chat over a virtual cup of coffee :)
-       */
-      // TODO: Add support for running the CLI against a local deployment
-      console.log("SBOM Updated");
-      const { manifestVersion, binaryUrl } = await getReleaseVersion();
-      await getCLI(manifestVersion, binaryUrl);
-      core.info("Sending request to Manifest Server");
-      execWrapper(`MANIFEST_API_KEY=${apiKey} ${manifestBinary} publish --ignore-validation=True --paths=${bomFilePath}`).then(r => {
-        core.info(`Manifest CLI response: ${r}`)
-      });
-    })
+  execWrapper(`SBOM_FILENAME=${bomFilePath} SBOM_OUTPUT=${output} SBOM_NAME=${name} SBOM_VERSION=${version} bash ${__dirname}/update-sbom.sh`).then(async () => {
+    /**
+     * At Manifest, we like to eat our own dogfood - you'll see some development code we use when testing our API and this action locally. You can safely ignore the below `if` statement - the `else` clause will always fire during normal production use of this action. We include our development code for both transparency to you and our own convenience.
+     * If you have a better idea or suggested improvements, fork/PR or ping us at engineering@manifestcyber.com and we'd love to chat over a virtual cup of coffee :)
+     */
+    // TODO: Add support for running the CLI against a local deployment
+    console.log("SBOM Updated");
+    const { manifestVersion, binaryUrl } = await getReleaseVersion();
+    await getCLI(manifestVersion, binaryUrl);
+    core.info("Sending request to Manifest Server");
+    execWrapper(`MANIFEST_API_KEY=${apiKey} ${manifestBinary} publish --ignore-validation=True --paths=${bomFilePath}`).then(r => {
+      core.info(`Manifest CLI response: ${r}`)
+    });
   });
 }
 catch (error) {
