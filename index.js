38b160e5aad4fe2788c620026ccee1cd49c66a9b
Weyland Chiang <71197790+wey-chiang@users.noreply.github.com>
2023-08-09 16:32:19 -0400

    MFST-607: Manifest CLI Support (#12)
    * MFST-606: Add support for publishing with the Manifest CLI
    
    * MFST-606: Updated action to run live
    
    * MFST-608: Add CLI supported inputs
    
    * MFST-607: nit: consistency in the action.yml
    
    * MFST-607: added core inputs and removed unnecessary execWrapper left in from testing
    
    * MFST-607: Added generators to description, made bomFilePath and sbom-publish non-required
    
    * MFST-607: Added support for SBOM generation
    
    * MFST-607: Changed workflow to run on this branch. Removed CycloneDX SBOM generation to test CLI generation
    
    * MFST-607: Shifted around logic for readability in the generate function and consistency for the SBOM artifact + SBOM published
    
    * MFST-607: added logging msg
    
    * MFST-607: added support for target path for sbom generation and flag for saving as an artifact
    
    * MFST-607: cleaning up, made input vars consistent and updated input vars
    
    * MFST-607: added default name for generated SBOM
    
    * MFST-607: fix for name
    
    * MFST-607: revert changes back on workflow back to main and added on push for all tags
    
    * MFST-607: updated optional inputs
    
    * MFST-607
     - Version bump
     - Added scripts for local testing SBOM generation for syft and trivy
     - Added semver comparison to support the `--source` flag
     - Fix for generatorFlags on `sbom` command
     - Fix for local testing `bomFilePath`
    
    * MFST-607: Add support for source and relationship flag and updated generate (sbom) command
    
    * chore: ran `yarn upgrade` to update dependencies
    
    * squash! chore: ran `yarn upgrade` to update dependencies
    
    * MFST-607: include node_modules
    
    * MFST-607: Made input `apiKey` non-required to enable users to only generate (and not publish) SBOMs
    
    * MFST-607: Add support for uploading SBOMs with labels

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const fs = require("fs");
-const https = require("https");
-const http = require("http");
 const core = require("@actions/core");
+const cache = require("@actions/tool-cache");
+const artifact = require("@actions/artifact");
+const artifactClient = artifact.create();
 const { exec } = require("child_process");
 const util = require("node:util");
+const semver = require("semver");
+
+const { Octokit } = require("@octokit/rest");
+const octokit = new Octokit();
+const owner = "manifest-cyber";
+const repo = "cli";
+const manifestBinary = "manifest";
+const tmpPath = "/tmp";
+const binaryPath = `${tmpPath}/${manifestBinary}`;
 
 const execPromise = util.promisify(exec);
+
+const validOutput = ["spdx-json", "cyclonedx-json"];
+const validGenerator = ["syft", "trivy", "cdxgen", "sigstore-bom", "spdx-sbom-generator", "docker-sbom"];
+const localTest = process.env.TEST_LOCALLY;
+const sourceFlagMinVer = "0.8.1"
+const labelsFlagMinVer = "0.9.1"
 
 async function execWrapper(cmd) {
   const { stdout, stderr, error } = await execPromise(cmd);
@@ -24,79 +40,147 @@ async function execWrapper(cmd) {
   }
 }
 
+// TODO: Add better support for different platforms
+async function getReleaseVersion() {
+  // Pull the latest version of the CLI
+  let release = await octokit.repos.getLatestRelease({ owner, repo });
+
+  let targetAsset = "manifest_linux_x86_64.tar.gz";
+  if (localTest && localTest === 'enabled') {
+    targetAsset = "manifest_darwin_x86_64.tar.gz";
+  }
+
+  let manifestVersion = release.data?.tag_name;
+  let binaryUrl = undefined;
+
+  for (let i = 0; i < release.data?.assets?.length || 0; i++) {
+    if (release.data.assets[i].name === targetAsset) {
+      binaryUrl = release.data.assets[i].browser_download_url;
+      break;
+    }
+  }
+
+  if (!binaryUrl) {
+    throw new Error("Could not find the latest release of the CLI");
+  }
+
+  return { manifestVersion, binaryUrl };
+}
+
+// TODO: Add support for caching the CLI
+async function getCLI(version, url) {
+  const dest = `${tmpPath}${url.substring(url.lastIndexOf("/"))}`
+  if (fileExists(`${binaryPath}/${manifestBinary}`)) {
+    core.info("Manifest CLI already exists, skipping download");
+    core.addPath(binaryPath);
+    return;
+  }
+
+  if (!fileExists(dest)) {
+    core.info(`Downloading the latest version of the CLI from ${url}`);
+    await cache.downloadTool(url, dest);
+  } else {
+    core.info('CLI tarball already exists, skipping download');
+  }
+
+  const binaryExtractedPath = await cache.extractTar(dest, binaryPath, 'xz');
+  core.addPath(binaryExtractedPath)
+}
+
+function shouldPublish(apiKey, publish) {
+  if (!apiKey) {
+    return false;
+  }
+
+  return publish !== "false";
+}
+
+function fileExists(filePath) {
+  return fs.existsSync(filePath);
+}
+
+function validateInput(output, generator) {
+  if (output && !validOutput.includes(output)) {
+    throw new Error(`Invalid output format: ${output}, expected to be one of ${validOutput}`);
+  }
+  if (generator && !validGenerator.includes(generator)) {
+    throw new Error(`Invalid generator: ${generator}, expected to be one of ${validGenerator}`);
+  }
+}
+
+// TODO: Add support for caching the generators that the CLI uses
+async function generateSBOM(targetPath, outputPath, outputFormat, sbomName, sbomVersion, generator, generatorFlags) {
+  if (fileExists(outputPath)) {
+    return;
+  }
+  validateInput(outputFormat, generator)
+  let sbomFlags = `--file=${outputPath.replace(/\.json$/, '')} --output=${outputFormat} --name=${sbomName} --version=${sbomVersion} --generator=${generator} --publish=false ${targetPath}`;
+  if (generatorFlags) {
+    sbomFlags = `${sbomFlags} -- ${generatorFlags}`;
+  }
+  core.info(`Generating SBOM using flags: ${sbomFlags}`);
+
+  await execWrapper(`${manifestBinary} install --generator ${generator}`).then(async () => {
+    core.info(`Installed ${generator}`);
+    await execWrapper(`${manifestBinary} sbom ${sbomFlags}`).then(() => { core.info(`SBOM Generated: ${outputPath}`) });
+  });
+  return outputPath;
+}
+
+
+// TODO: Add support for running the CLI against a local deployment
 try {
   const apiKey = core.getInput("apiKey");
-  const bomFilePath = core.getInput("bomFilePath");
-  const output = core.getInput("sbom-output");
-  const name = core.getInput("sbom-name");
-  const version = core.getInput("sbom-version");
+  core.setSecret(apiKey);
+  const targetPath = core.getInput("path") || __dirname;
+  const name = core.getInput("bomName") || `${process.env.GITHUB_REPOSITORY?.replace(`${process.env.GITHUB_REPOSITORY_OWNER}/`, "")}`;
 
-  const relationship = core.getInput("relationship");
+  const bomFilePath = core.getInput("bomFilePath") || `${name}.json`;
+  const version = core.getInput("bomVersion");
+  const output = core.getInput("bomOutput");
+  const generator = core.getInput("bomGenerator");
+  const artifact = core.getInput("bomArtifact");
+  const publish = core.getInput("bomPublish");
+  const generatorFlags = core.getInput("bomGeneratorFlags");
   const source = core.getInput("source");
+  const relationship = core.getInput("relationship");
+  const labels= core.getInput('bomLabels') || '';
 
-  execWrapper(`SBOM_FILENAME=${bomFilePath} SBOM_OUTPUT=${output} SBOM_NAME=${name} SBOM_VERSION=${version} bash ${__dirname}/update-sbom.sh`).then(() => {
-    const bomContents = fs.readFileSync(bomFilePath);
-    const base64BomContents = Buffer.from(bomContents).toString("base64");
-
-    const payload = {
-      base64BomContents, // Incoming file Buffer
-      relationship,// 'first' or 'third'-party
-      filename: bomFilePath, // File name/path. Optional. Used for logging/debugging purposes.
-      source, // This is stored and visible to you in the Manifest app.
-    };
-
-    const postData = JSON.stringify(payload);
-
-    const requestOptions = {
-      method: "PUT",
-      headers: {
-        "Authorization": `Bearer ${apiKey}`,
-        "Content-Type": "application/json",
-        "Content-Length": Buffer.byteLength(postData),
-      },
-    };
-
-    console.log("Sending request to Manifest Server");
-    let req = null;
-
-    /**
-     * At Manifest, we like to eat our own dogfood - you'll see some development code we use when testing our API and this action locally. You can safely ignore the below `if` statement - the `else` clause will always fire during normal production use of this action. We include our development code for both transparency to you and our own convenience.
-     * If you have a better idea or suggested improvements, fork/PR or ping us at engineering@manifestcyber.com and we'd love to chat over a virtual cup of coffee :)
-     */
-    if (process.env.TEST_LOCALLY && process.env.TEST_LOCALLY === 'enabled') {
-      req = http.request(`http://local.manifestcyber.com:8081/v1/sbom/upload`, requestOptions, (res) => {
-        const statusCode = res.statusCode;
-        if (statusCode >= 200 && statusCode < 300) {
-          console.log("[DEV] Uploaded to Manifest Server");
-        } else {
-          core.setFailed(
-            "[DEV] Failed to upload:" + res.statusCode + " " + res.statusMessage
-          );
-        }
+  /**
+   * At Manifest, we like to eat our own dogfood - you'll see some development code we use when testing our API and this action locally. We include our development code for both transparency to you and our own convenience.
+   * If you have a better idea or suggested improvements, fork/PR or ping us at engineering@manifestcyber.com and we'd love to chat over a virtual cup of coffee :)
+   */
+  getReleaseVersion().then(async ({ manifestVersion, binaryUrl }) => {
+    getCLI(manifestVersion, binaryUrl).then(async () => {
+      generateSBOM(targetPath, bomFilePath, output, name, version, generator, generatorFlags).then(async (outputPath) => {
+        execWrapper(`SBOM_FILENAME=${bomFilePath} SBOM_OUTPUT=${output} SBOM_NAME=${name} SBOM_VERSION=${version} bash ${__dirname}/update-sbom.sh`).then(async () => {
+          console.log("SBOM Updated");
+          if (outputPath && artifact === "true") {
+            const upload = await artifactClient.uploadArtifact("sbom", [outputPath], outputPath.substring(0, outputPath.lastIndexOf("/")));
+            core.info(`SBOM uploaded to GitHub as an artifact: ${upload}`);
+          }
+          if (shouldPublish(apiKey, publish)) {
+            let publishCommand = `MANIFEST_API_KEY=${apiKey} ${manifestBinary} publish --ignore-validation=True --paths=${bomFilePath} --source=${source} --relationship=${relationship}`;
+            const mVer = semver.coerce(manifestVersion)
+            if (mVer && semver.gte(mVer, sourceFlagMinVer)) {
+              publishCommand = `${publishCommand} --source=github-action`;
+            } else {
+              core.warning(`The version of the CLI (${manifestVersion}) does not support the \`--source\` flag. Please upgrade to v0.8.1 or later.`);
+            }
+            if (mVer && labels && semver.gte(mVer, labelsFlagMinVer)) {
+              publishCommand = `${publishCommand} --labels=${labels.split(' ').join('-')}`;
+            } else if (labels) {
+              core.warning(`The version of the CLI (${manifestVersion}) does not support the \`--labels\` flag. Please upgrade to v0.9.1 or later.`);
+            }
+            core.info("Sending request to Manifest Server");
+            await execWrapper(publishCommand);
+          } else {
+            core.info('No API Key provided, skipping publish')
+          }
+        });
       });
-    }
-
-    else {
-      req = https.request(`https://api.manifestcyber.com/v1/sbom/upload`, requestOptions, (res) => {
-        const statusCode = res.statusCode;
-        if (statusCode >= 200 && statusCode < 300) {
-          console.log("Uploaded to Manifest Server");
-        } else {
-          core.setFailed(
-            "Failed to upload:" + res.statusCode + " " + res.statusMessage
-          );
-        }
-      });
-    }
-
-    req.on("error", (e) => {
-      console.error(`Problem with request: ${e.message}`);
-      core.setFailed(e.message);
     });
-
-    req.write(postData);
-    req.end();
-  })
+  });
 }
 catch (error) {
   core.setFailed(error.message);
