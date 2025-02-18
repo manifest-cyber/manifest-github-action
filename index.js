9c0d189213cf890253df34b6a8deae2bc07cb569
Ori Avraham <ori@manifestcyber.com>
2025-02-18 15:04:04 +0100

    chore: update dependencies and parlay to ecosystems [MFST-4945]

--- index.js
+++ index.js
@@ -153,11 +153,15 @@ async function generateSBOM(
   }
 
   if (generator === "syft" && generatorVersion === "") {
-    generatorVersion = "v1.16.0";
+    generatorVersion = "v1.19.0";
   }
 
   if (generator === "trivy" && generatorVersion === "") {
-    generatorVersion = "v0.57.0";
+    generatorVersion = "v0.59.1";
+  }
+
+  if (generator === "cdxgen" && generatorVersion === "") {
+    generatorVersion = "v11.1.8";
   }
 
   const installCommand =
@@ -235,7 +239,7 @@ try {
   const source = core.getInput("source");
   const relationship = core.getInput("relationship");
   const active = core.getInput("active"); // Active: true, false
-  const enrich = core.getInput("enrich"); // Enrichment: NONE, PARLAY, PARLAY_ADDITIVE, etc. If not present org default is used.
+  const enrich = core.getInput("enrich"); // Enrichment: NONE, or ECOSYSTEMS, etc. If not present org default is used.
   const assetLabels =
     core.getInput("sbomLabels") ||
     core.getInput("bomLabels") ||
